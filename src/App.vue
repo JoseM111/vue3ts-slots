ed1773c8576f9b77040ebcb7ad6020a853503cc5
Jose Martinez <alias_one11@outlook.com>
2021-07-20 01:35:56 +0200

    1. complete

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,112 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-</template>
+<!-- App.vue -->
 
+// 🌀🌀💻 SCRIPT 💻🌀🌀
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+import { defineComponent } from 'vue'
+import AppFormComponent from '@/components/app-form/AppForm.component.vue'
+// ⚫️⚫️☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+  components: { AppFormComponent },
+})
 </script>
+<!-- 🌀🌀💻================================================= -->
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+// 🌀🌀💻 HTML 💻🌀🌀
+<template>
+  <div class="formContainer">
+    
+    <!--⚫️ Will default to text, since no children were passed ⚫️-->
+    <!--⚫️ Defaults if no children passed to the component ⚫️-->
+    <h1>
+      <span class="defaultText">
+        <b>NO FORM TO RENDER</b>
+      </span>
+    </h1>
+    
+    <!--⚫️ BUTTON TEXT ⚫️-->
+    <AppFormComponent>
+      <!--⚫️ help-paragraph ⚫️-->
+      <template v-slot:help>
+        <p><b>This is some help text.</b></p>
+      </template>
+
+      <!--⚫️ fields-inputs ⚫️-->
+      <template v-slot:fields>
+        <input type="text" placeholder="email">
+        <input type="text" placeholder="username">
+        <input type="password" placeholder="password">
+      </template>
+
+      <!--⚫️ buttons ⚫️-->
+      <template v-slot:buttons>
+        <button type="submit" class="btnText">
+          Submit
+        </button>
+      </template>
+    </AppFormComponent>
+
+    <!--⚫️ BUTTON CONTACT ⚫️-->
+    <AppFormComponent>
+      <!--⚫️ help-paragraph ⚫️-->
+      <template v-slot:help>
+        <p><b>Contact help text.</b></p>
+      </template>
+
+      <!--⚫️ fields-inputs ⚫️-->
+      <template v-slot:fields>
+        <input type="text" placeholder="name">
+        <input type="text" placeholder="message">
+      </template>
+
+      <!--⚫️ buttons ⚫️-->
+      <template v-slot:buttons>
+        <button type="submit" class="btnsContacts">
+          Submit
+        </button>
+      </template>
+    </AppFormComponent>
+    
+  </div>
+  
+  
+</template>
+<!-- 🌀🌀💻================================================ -->
+
+// 🌀🌀💻 STYLES 💻🌀🌀
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import 'src/components/app-form/StyledAppForm.scss';
+/*⚫️⚫️☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰ */
+//: - ∂
+@mixin bgBlack() {
+  position: absolute;
+  width: 100%;
+  min-height: 100%;
+  left: 0;
+  top: 0;
+  background: #000;
+}
+
+@mixin alignContentCenter() {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1ch;
+}
+
+/*⚫️⚫️☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰ */
+.formContainer {
+  @include bgBlack;
+  @include alignContentCenter;
+  font-family: "Amazon Ember Cd RC", sans-serif;
+  // ⚫️⚫️☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰
+  h1 {
+    font-family: "Amazon Ember Cd RC", sans-serif;
+    color: white;
+  }
 }
 </style>
+<!-- 🌀🌀💻================================================ -->
+
---
+++ src/components/app-form/AppForm.component.vue
@@ -0,0 +1,45 @@
+<!-- FormComponent.vue -->
+
+// 🌀🌀💻 SCRIPT 💻🌀🌀
+<script lang="ts">
+import { defineComponent } from 'vue'
+// ⚫️⚫️☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰
+export default defineComponent({
+  name: 'AppFormComponent',
+})
+</script>
+<!-- 🌀🌀💻================================================= -->
+
+// 🌀🌀💻 HTML 💻🌀🌀
+<template>
+  
+  <form action="">
+    
+    <!--⚫️ Named slots ⚫️-->
+    <div class="help">
+      <slot name="help"></slot>
+    </div>
+    
+    <div class="fields">
+      <slot name="fields"></slot>
+    </div>
+    
+    <div class="buttons">
+      <slot name="buttons"></slot>
+    </div>
+
+    <!--⚫️ Able to have children | Will default in child components ⚫️-->
+    <slot></slot>
+  
+  </form>
+  
+</template>
+<!-- 🌀🌀💻================================================ -->
+
+// 🌀🌀💻 STYLES 💻🌀🌀
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import 'StyledAppForm';
+</style>
+<!-- 🌀🌀💻================================================ -->
+
